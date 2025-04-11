92dd8cc4dfd092c0f4c29f11760a4715b6c8aefd
J <knowisdom314@gmail.com>
2025-04-11 17:53:19 +0900

    Initial Commit

---
+++ diver-break/diver_breakApp.swift
@@ -0,0 +1,17 @@
+//
+//  diver_breakApp.swift
+//  diver-break
+//
+//  Created by J on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct diver_breakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
