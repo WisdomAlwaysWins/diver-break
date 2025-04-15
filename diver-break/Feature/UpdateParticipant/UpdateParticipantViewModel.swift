57d389f8369145c58e9950c3bcd8a3ce741c40ea
J <knowisdom314@gmail.com>
2025-04-15 10:00:59 +0900

    [추가] 팀원 추가 UI 구현 및 메인 화면 연결

---
+++ diver-break/Feature/UpdateParticipant/UpdateParticipantViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateParticipantViewModel.swift
+//  diver-break
+//
+//  Created by J on 4/14/25.
+//
+
+import SwiftUI
+
+struct UpdateParticipantViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateParticipantViewModel()
+}
